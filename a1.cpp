b5fd9ffe1ff864aecf0c3d5c8975b44794bd57b5
Srivatsan Iyer <srriyer@indiana.edu>
2016-01-29 18:23:43 -0500

    Add general convolution

--- a1.cpp
+++ a1.cpp
@@ -135,8 +135,44 @@ SDoublePlane convolve_separable(const SDoublePlane &input, const SDoublePlane &r
 //
 SDoublePlane convolve_general(const SDoublePlane &input, const SDoublePlane &filter)
 {
+	//Requires the dimension of the filter to be smaller than the input.
+	//Switch the parameters otherwise.
+	
+	if ((input.rows() - filter.rows()) * (input.cols() - filter.cols()) < 0) {
+		throw "Mismatched dimensions.";
+	}
+	if (input.rows() < filter.rows()) {
+		return convolve_general(filter, input);
+	}
+	
+	if (filter.rows()%2 == 0 || filter.cols()%2 == 0){
+		std::cerr<<"Rows/Columns in filter: "<<filter.rows()<<"/"<<filter.cols()<<std::endl;
+		throw "Expected an odd number of rows and columns in the filter";
+	}
+	//From here, we have ensured dimensions of input is larger than the filter.
+	
 	SDoublePlane output(input.rows(), input.cols());
 
+	int filter_rows_num = filter.rows(),
+		filter_cols_num = filter.cols(),
+		image_rows_num = input.rows(),
+		image_cols_num = input.cols();
+	int start_row = filter.rows()/2,
+		start_col = filter.cols()/2,
+		end_row = input.rows() - start_row,
+		end_col = input.cols() - start_col;
+	for (int i = start_row; i < end_row; i++) {
+		for (int j = start_col; j < end_col; j++) {
+			int sum = 0;
+			for (int p = 0; p < filter_rows_num; p++) {
+				for (int q = 0; q < filter_cols_num; q++) {
+					sum += input[i-filter_rows_num/2+p][j-filter_cols_num/2+q] * filter[p][q];
+				}
+			}
+			output[i+filter_rows_num/2][j+filter_cols_num/2] = sum;
+		}
+	}
+	
 	return output;
 }
 
@@ -145,12 +181,9 @@ SDoublePlane convolve_general(const SDoublePlane &input, const SDoublePlane &fil
 // 
 SDoublePlane sobel_gradient_filter(const SDoublePlane &input, bool _gx)
 {
-	double x = input[5][4];
-
 	SDoublePlane output(input.rows(), input.cols());
 
 	// Implement a sobel gradient estimation filter with 1-d filters
-
 
 	return output;
 }
