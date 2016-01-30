57edf892c95d909b51b5390c6efecb2feb5bf774
Srivatsan Iyer <srriyer@indiana.edu>
2016-01-29 22:19:22 -0500

    Moving Symbols enum to a different header file.

---
+++ Symbols.h
@@ -0,0 +1,19 @@
+#ifndef SYMBOLS_H
+#define SYMBOLS_H
+
+typedef enum {
+	NOTEHEAD=0,
+	QUARTERREST=1,
+	EIGHTHREST=2
+} Type;
+
+
+class DetectedSymbol {
+public:
+	int row, col, width, height;
+	Type type;
+	char pitch;
+	double confidence;
+};
+
+#endif
