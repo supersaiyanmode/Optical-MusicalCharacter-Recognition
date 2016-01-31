73ad04112ebf940ff3222cc7e37983a967ad612b
CYRIL MICHAEL SHELKE <cshelke@jerboa.soic.indiana.edu>
2016-01-31 15:30:37 -0500

    a few corrections after testing

--- utils.cpp
+++ utils.cpp
@@ -1,45 +1,69 @@
 #include "utils.h"
 
+
 // Convolve an image with a separable convolution kernel
 //
 SDoublePlane convolve_separable(const SDoublePlane &input, const SDoublePlane &row_filter, const SDoublePlane &col_filter)
 {
-	
 	SDoublePlane output(input.rows(), input.cols());
-	int num_filter_rows = row_filter.rows();
-	int num_filter_cols = col_filter.cols();
-	int num_input_cols = input.rows();
-	int num_input_rows = input.cols();
-	int sum;
-	int k;
-	for(int i=0; i<= (num_input_rows - num_filter_rows);i++)
+	int num_filter_rows = col_filter.rows();
+	int num_filter_cols = row_filter.cols();
+	int num_input_cols = input.cols();
+	int num_input_rows = input.rows();
+	int sum = 0;
+	int k=0;
+
+	if ((input.rows() - col_filter.rows()) * (input.cols() - row_filter.cols()) < 0) {
+		throw "Mismatched dimensions.";
+	}
+		
+	if (col_filter.rows()%2 == 0 || row_filter.cols()%2 == 0){
+		throw "Expected an odd number of rows and columns in the filter";
+	}
+	/*for(int p=0;p<5;p++)
+			for(int q=0;q<5;q++)
+				{
+					output[p][q] = 0;
+					if (p!= 0 && q!= 0 && p!=4 && q!=4)
+					{
+						input[p][q] = 16;			
+					}
+					else
+						input[p][q] = 0;
+				}*/
+	
+	//convolving first by the column filter	
+	for(int i=0; i<= (num_input_rows - num_filter_rows); i++)
 	{
 		for(int m=0; m < num_input_cols; m++)
 		{
 			sum = 0;
-			k = m;
-			for(int j=0;j<num_filter_rows;j++)
+			k = i;
+			for(int j=0; j<num_filter_rows; j++)
 			{
-				sum += row_filter[j][0] * input[k][i];
+				sum += col_filter[j][0] * input[k][m];
 				k++;
 			}
-			output[(k+m)/2][m] = sum;
+			output[(k+i)/2][m] = sum;
 		}
 	}
 	
+	//convolving by row filter
 	for(int i=0; i< num_input_rows;i++)
-	{
-		for(int m=0; m <= (num_input_cols - num_filter_cols); m++)	
-		{	sum = 0;
-			k = m;
-			for(int j=0;j<num_filter_cols;j++)
-			{
-				sum += row_filter[0][j] * input[i][k];
-				k++;
+		{
+			for(int m=0; m <= (num_input_cols - num_filter_cols); m++)	
+			{	sum = 0;
+				k = m;
+				for(int j=0;j<num_filter_cols;j++)
+				{
+					sum += row_filter[0][j] * output[i][k];
+					k++;
+				}
+				output[i][(k+m)/2] = sum;
 			}
-			output[i][(k+m)/2] = sum;
 		}
-	}
+
+
 	return output;
 }
 
@@ -62,7 +86,7 @@ SDoublePlane convolve_general(const SDoublePlane &input, const SDoublePlane &fil
 	}
 	//From here, we have ensured dimensions of input is larger than the filter.
 	
-	SDoublePlane output(input.rows(), input.cols());
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              		SDoublePlane output(input.rows(), input.cols());
 
 	int 	filter_rows_num = filter.rows(),
 			filter_cols_num = filter.cols(),
--- utils.h
+++ utils.h
@@ -4,5 +4,6 @@
 #include "SImage.h"
 
 SDoublePlane convolve_general(const SDoublePlane& input, const SDoublePlane& filter);
+SDoublePlane convolve_separable(const SDoublePlane &input, const SDoublePlane &row_filter, const SDoublePlane &col_filter);
 
 #endif
