de24ee6ab8e99dd96b880408e5d9a7985fe46f55
Srivatsan Iyer <srriyer@indiana.edu>
2016-02-05 08:19:46 -0500

    Adding basic canny edge detector

---
+++ Canny.cpp
@@ -0,0 +1,119 @@
+#include<cmath>
+#include <iostream>
+
+#include "Canny.h"
+#include "utils.h"
+#include "Kernel.h"
+#include "SImageIO.h"
+
+#define DIR_NORTH 0
+#define DIR_NORTH_EAST 1
+#define DIR_EAST 2
+#define DIR_NORTH_WEST 3
+
+#define BETWEEN(X,Y,Z) (((X) <= (Y)) && ((Y) <= (Z)))
+#define MAXIMA(X,Y,Z) (((x) < (Y)) && ((Y) > (Z)))
+namespace {
+	
+	SDoublePlane sobelx_kernel = load_kernel("kernels/sobel3x");
+	SDoublePlane sobely_kernel = load_kernel("kernels/sobel3y");
+}
+
+SDoublePlane canny(const SDoublePlane& image, double low_thresh, double high_thresh) {
+	/*
+	 * Reference Used:http://www.inf.u-szeged.hu/ssip/2003/lectures/chetverikov/edge_detection.pdf
+	 * Slides #16-#21
+	 */
+
+	SDoublePlane sobelx = convolve_general(image, sobelx_kernel);
+	SDoublePlane sobely = convolve_general(image, sobely_kernel);
+	SDoublePlane magnitude(image.rows(), image.cols());
+	SDoublePlane output = image;
+	_DTwoDimArray<int> angle(image.rows(), image.cols());
+
+	SDoublePlane temp = sobelx + sobely;
+	SImageIO::write_png_file("sobel.png", temp, temp, temp);
+
+	for (int i=0; i<image.rows(); i++) {
+		for (int j=0; j<image.cols(); j++) {
+			magnitude[i][j] = std::sqrt(sobelx[i][j]*sobelx[i][j] + 
+										sobely[i][j]*sobely[i][j]);
+			double ang = std::atan2(sobely[i][j], sobely[i][j]) * 180.0;
+			if (BETWEEN(-22.5,ang,22.5) || (BETWEEN(157.5,ang,180) && BETWEEN(-180,ang,-157.5)))
+				angle[i][j] = DIR_EAST;
+			else if (BETWEEN(22.5,ang,67.5) || BETWEEN(-157.5,ang,-112.5))
+				angle[i][j] = DIR_NORTH_EAST;
+			else if (BETWEEN(67.5,ang,112.5) || BETWEEN(-112.5,ang,-67.5))
+				angle[i][j] = DIR_NORTH;
+			else if (BETWEEN(112.5,ang,157.5) || BETWEEN(-67.5,ang,-22.5))
+				angle[i][j] = DIR_NORTH_WEST;
+
+			if (magnitude[i][j] < low_thresh) { 
+				//std::cout<<"Suppressed because lower than threshold: "<<i<<","<<j<<std::endl;
+				output[i][j] = 0;
+			} else {
+				output[i][j] = 255;
+			}
+		}
+	}
+
+	SImageIO::write_png_file("_magnitude.png", magnitude, magnitude, magnitude);
+
+	for (int i=0; i<output.rows(); i++) {
+		for (int j=0; j<output.cols(); j++) {
+			if (output[i][j] < 1.0) {
+				output[i][j] = 0;
+				continue;
+			}
+			if (angle[i][j] == DIR_NORTH) {
+				if (i > 0 && magnitude[i-1][j] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}
+				else if (i < image.rows() - 1 && magnitude[i+1][j] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}
+				else {
+					//std::cout<<"Found maximum at: "<<i<<","<<j<<std::endl;
+				}
+			}
+			else if (angle[i][j] == DIR_NORTH_EAST) {
+				if (i > 0 && j < image.cols() - 1 && magnitude[i-1][j+1] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}
+				else if (i < image.rows() - 1 && j > 0 && magnitude[i+1][j-1] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}else {
+					//std::cout<<"Found maximum at: "<<i<<","<<j<<std::endl;
+				}
+
+			}
+			else if (angle[i][j] == DIR_EAST) {
+				if (j < image.cols() - 1 && magnitude[i][j+1] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}
+				else if (j > 0 && magnitude[i][j-1] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}else {
+					//std::cout<<"Found maximum at: "<<i<<","<<j<<std::endl;
+				}
+
+			}
+			else if (angle[i][j] == DIR_NORTH_WEST) {
+				if (i > 0 && j > 0 && magnitude[i-1][j-1] > magnitude[i][j]) {
+					output[i][j] = 0;
+				}
+				else if (i < image.rows() - 1 && j < image.cols() - 1 && magnitude[i+1][j+1] < magnitude[i][j]) {
+					output[i][j] = 0;
+				}else {
+					//std::cout<<"Found maximum at: "<<i<<","<<j<<std::endl;
+				}
+
+			}
+		}
+	}
+
+	std::cout<<"Suppressed pixel count: "<<image_sum(threshold(image + output*-1,-1,THRESH_RETAIN, 1))<<std::endl;
+
+	return output;
+	
+}
---
+++ Canny.h
@@ -0,0 +1,8 @@
+#ifndef __CANNY_H
+#define __CANNY_H
+
+#include "SImage.h"
+
+SDoublePlane canny(const SDoublePlane& image, double, double);
+
+#endif
