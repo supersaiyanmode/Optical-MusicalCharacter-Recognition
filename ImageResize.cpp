2f1e9989690a7c79f1e04da4d8188ceec3719780
Srivatsan Iyer <srriyer@indiana.edu>
2016-02-09 22:40:18 -0500

    Coarses resize.

--- ImageResize.cpp
+++ ImageResize.cpp
@@ -6,7 +6,8 @@
 
 SDoublePlane get_template(int template_num, double staff_height) {
 	std::stringstream ss;
-	ss<<"templates/template"<<template_num<<"-"<<int(staff_height *100 / 11.0)<<".png";
+	int res = ((int)(staff_height * 5 / (11.0) + 0.5) + 5) * 10;
+	ss<<"templates/template"<<template_num<<"-"<<res<<".png";
 	std::cout<<"Loading Template: "<<ss.str()<<std::endl;
 	return SImageIO::read_png_file(ss.str().c_str());
 }
